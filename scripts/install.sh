3bfe7df5ba0bd44ac31e3a916567cea763ea6c76
HanmaDevin <devinbc14@gmail.com>
2025-01-23 01:35:39 +0100

    updated script

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,82 +43,6 @@ configure_git() {
   fi
 }
 
-add_ssh_key() {
-  read -p "Ready to add ssh-key? (y/n): " sshready
-  if [[ $sshready == "y" ]]; then
-    echo "Do not change the default file location and name in the following!!!"
-    read -p "What is your email address? " email
-    ssh-keygen -t ed25519 -C "$email"
-    ssh-add ~/.ssh/id_ed25519
-    echo "Paste the following content in your ssh-key section on GitHub:"
-    cat ~/.ssh/id_ed25519.pub
-  fi
-}
-
-#add_wallpaper() {
-  #if [[ ! -d "$HOME/Pictures/wallpaper" ]]; then
-    #mkdir -p "$HOME/Pictures/wallpaper"
-  #fi
-  #cp -a "$PWD/wallpaper/." "$HOME/Pictures/wallpaper/" && echo "Finished adding wallpapers!" || echo "Failed to add wallpapers."
-#}
-
-#add_configs() {
-	#echo "Adding geany colorschemes"
-
-	#if [[ ! -d "$HOME/.config/geany/colorschemes" ]]; then
-        #mkdir -p "$HOME/.config/geany/colorschemes"
-    #fi
-
-	#cp -a "$PWD/geany/." "$HOME/.config/geany/colorschemes"
-
-	#echo "Finished!"
-
-    #echo "Adding btop config"
-
-    #if [[ ! -d "$HOME/.config/btop" ]]; then
-        #mkdir -p "$HOME/.config/btop"
-    #fi
-
-    #cp "$PWD/btop/btop.conf" "$HOME/.config/btop"
-
-    #echo "Finished!"
-
-    #echo "Adding kitty config"
-
-    #if [[ ! -d "$HOME/.config/kitty" ]]; then
-        #mkdir -p "$HOME/.config/kitty"
-    #fi
-
-    #cp -a "$PWD/kitty/." "$HOME/.config/kitty"
-
-    #echo "Finished!"
-
-    ## adding neovim plugin
-    #echo "Adding neovim plugin"
-    #echo "Running neovim script"
-
-    #bash "$PWD/scripts/neovim.sh"
-
-    #if [[ ! -d "$HOME/.config/nvim/lua/plugins/" ]]; then
-      #mkdir -p "$HOME/.config/nvim/lua/plugins/"
-    #fi
-
-    #cp "$PWD/neovim/lazygit.lua" "$HOME/.config/nvim/lua/plugins/"
-
-    #echo "Finished!"
-
-    #add_wallpaper
-    
-    #install_nitch
-
-    #echo "Adding font and cursor"
-
-    #sudo cp "$PWD/Fonts/MesloLGS NF Regular.ttf" "/usr/share/fonts/"
-    #sudo cp -r "$PWD/Cursor/Bibata-Modern-Ice/" "/usr/share/icons/"
-
-    #echo "Finished!"
-#}
-
 case $answer in
   #1)
     #install_packages "apt" "zip" "ufw" "zsh" "unzip" "stow" "wget" "lutris" "curl" "neovim" "eza" "btop" "gamemode" "mangohud" "zoxide" "fzf" "bat" "kitty" "geany" "geany-plugins"
@@ -170,8 +94,7 @@ case $answer in
     configure_git
     add_ssh_key
     config_ufw
-
-	#add_configs
+    install_nitch
     
     echo "adding zshrc"
 
