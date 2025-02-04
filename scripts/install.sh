fc1a8e16d27e5001184d29d558fc30804ed54976
HanmaDevin <devinbc14@gmail.com>
2025-02-04 15:14:26 +0100

    added package

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 install_packages() {
-  packages=("zip" "ntfs-3g" "tree" "discord" "lazygit" "ufw" "zsh" "unzip" "wget" "stow" "curl" "yazi" "neovim" "eza" "btop" "gamemode" "steam" "mangohud" "zoxide" "fzf" "bat" "kitty" "geany" "geany-plugins" "tmux" "jdk23-openjdk" "docker" "ripgrep" "rofi" "feh" "wireplumber" "pipewire-pulse" "polybar" "cargo" "fd" "pavucontrol" "alsa-utils" "xclip")
+  packages=("zip" "ntfs-3g" "tree" "discord" "lazygit" "ufw" "zsh" "unzip" "wget" "stow" "curl" "yazi" "neovim" "eza" "btop" "gamemode" "steam" "mangohud" "zoxide" "fzf" "bat" "kitty" "geany" "geany-plugins" "tmux" "jdk23-openjdk" "docker" "ripgrep" "rofi" "feh" "wireplumber" "pipewire-pulse" "polybar" "cargo" "fd" "pavucontrol" "alsa-utils" "xclip" "python-i3ipc")
 
   for package in "${packages[@]}"; do
 		sudo pacman -S "$package" --noconfirm || { echo "Failed to install $package"; exit 1; }
